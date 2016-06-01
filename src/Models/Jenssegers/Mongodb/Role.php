7e316b923bd16dcbcb4cc06fe0402255bec26d99
Ademir Mazer Junior <mazer@uepg.br>
2016-06-01 12:02:18 -0300

    Fix namespaces

--- src/Models/Jenssegers/Mongodb/Role.php
+++ src/Models/Jenssegers/Mongodb/Role.php
@@ -3,12 +3,12 @@
 namespace Winponta\Cani\Models\Jenssegers\Mongodb;
 
 use Jenssegers\Mongodb\Eloquent\Model;
-use Winponta\Cani\Contracts\Role;
+use Winponta\Cani\Contracts\Role as RoleContract;
 
 /**
  * Base Role class to use with Jenssegers/Mongodb driver.
  */
-class Role extends Model implements Role {
+class Role extends Model implements RoleContract {
     protected $table = 'cani_roles';
 
     protected $fillable = ['name', 'label', 'description'];
