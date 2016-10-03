a1a35b8729dca91ca4839740e3d41ac536f569cc
João Henrique Moretti Pellissari <joaoh@Joaos-MacBook-Pro.local>
2016-10-03 20:00:21 -0300

    Removed routes and views from CaniServiceProvider

--- src/Providers/CaniServiceProvider.php
+++ src/Providers/CaniServiceProvider.php
@@ -21,21 +21,9 @@ class CaniServiceProvider extends ServiceProvider {
     }
 
     private function initializeResources() {
-        // incluindo as rotas padrões
-        include __DIR__ . '/../Http/routes.php';
-
-        // registra o caminho das visões deste pacote
-        $this->loadViewsFrom(__DIR__ . '/views', 'cani');
-
         $this->mergeConfigFrom(
                 __DIR__ . '/../config/cani.php', 'cani'
         );
-
-        // define um diretório para publicar as visões e configurações caso o desenvolvedor
-        // usuário deste pacote deseje alterá-las na sua aplicação
-        $this->publishes([
-            __DIR__ . '/views' => base_path('resources/views/vendor/winponta/cani'),
-                ], 'views');
 
         $this->publishes([
             __DIR__ . '/../config' => config_path(),
@@ -52,11 +40,6 @@ class CaniServiceProvider extends ServiceProvider {
             //return new \Winponta\Cani\Cani($app, $app['cani.models.role'], $app['cani.models.permission']);
             return new \Winponta\Cani\Cani($app);
         });
-        
-        //  $this->app->make('Winponta\Cani\Http\Controllers\CaniController');
-//        $this->app->singleton('Riak\Contracts\Connection', function ($app) {
-//            return new Connection(config('riak'));
-//        });
     }
 
 }
